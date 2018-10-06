c772a716bc23a0d3bcdad08dafd7fb6d1dfecff9
Nestor Velasquez <nestor.velasquez.santos@gmail.com>
2018-10-06 16:21:05 -0400

    Adding list

--- ImplementPrism/ViewModels/MainPageViewModel.cs
+++ ImplementPrism/ViewModels/MainPageViewModel.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using Prism.Commands;
 using Prism.Mvvm;
 
 namespace ImplementPrism.ViewModels
 {
     public class MainPageViewModel : BindableBase
     {
+        ObservableCollection<string> _list;
+        public ObservableCollection<string> List
+        {
+            get {
+                return _list;
+            }
+            set {
+                SetProperty(ref _list, value);
+            }
+        }
+
+        public DelegateCommand AddCommand { get; set; }
+        int index = 1;
+
         public MainPageViewModel()
         {
+            List = new ObservableCollection<string>();
+            AddCommand = new DelegateCommand(() => {
+                List.Add("Index No: " + index);
+                index++;
+            });
         }
     }
 }
